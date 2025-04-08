a5cdf511db61ab82e8e26b689abf7b1c084e8e33
TefaEspaña <estefaniaespana18@gmail.com>
2025-04-08 13:19:23 -0600

    Parte de la A a la G

---
+++ script.js
@@ -0,0 +1,27 @@
+function a(){
+    document.getElementById("fotoa").src="avion.jpg";
+}
+
+function b(){
+    document.getElementById("fotob").src="buzon.jpg";
+}
+
+function c(){
+    document.getElementById("fotoc").src="casa.jpg";
+}
+
+function d(){
+    document.getElementById("fotod").src="dedos.jpg";
+}
+
+function e(){
+    document.getElementById("fotoe").src="elefente.jpg";
+}
+
+function f(){
+    document.getElementById("fotof").src="foca.jpg";
+}
+
+function g(){
+    document.getElementById("fotog").src="gallina.jpg";
+}
