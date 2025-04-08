eebf0f8eeb39d1be765360471bb1b24aa0f4053f
Michaell Gonzalez Garcia <miagonzalezg@udistrital.edu.co>
2025-04-08 14:35:26 -0500

    letra de la h a n

---
+++ script.js
@@ -0,0 +1,21 @@
+function h(){
+    document.getElementById("fotoh").src="Hoja.jpg";
+}
+function i(){
+    document.getElementById("fotoi").src="Iglesia.jpg";
+}
+function j(){
+    document.getElementById("fotoj").src="Jirafa.png";
+}
+function k(){
+    document.getElementById("fotok").src="Koala.jpg";
+}
+function l(){
+    document.getElementById("fotol").src="Luna.jpg";
+}
+function m(){
+    document.getElementById("fotom").src="Manzana.webp";
+}
+function n(){
+    document.getElementById("foton").src="Naranja.jpg";
+}
