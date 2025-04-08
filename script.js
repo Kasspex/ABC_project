a149a4ee29affe3fac0288237a68f8547e54bd16
Kasspex <feliphomezparda@gmail.com>
2025-04-08 18:10:28 -0500

    Letras de la A a la G

---
+++ script.js
@@ -0,0 +1,27 @@
+function a(){
+    document.getElementById("fotoa").src="./images/avion.jpg";
+}
+
+function b(){
+    document.getElementById("fotob").src="./images/buzon.jpg";
+}
+
+function c(){
+    document.getElementById("fotoc").src="./images/casa.jpg";
+}
+
+function d(){
+    document.getElementById("fotod").src="./images/dedos.jpg";
+}
+
+function e(){
+    document.getElementById("fotoe").src="./images/elefente.jpg";
+}
+
+function f(){
+    document.getElementById("fotof").src="./images/foca.jpg";
+}
+
+function g(){
+    document.getElementById("fotog").src="./images/gallina.jpg";
+}
